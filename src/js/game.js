6bc4684be594b2a83fbe7929a5979127de3efd76
Patrik <patrik.podhorsky@gmail.com>
2020-03-28 16:14:32 +0100

    Initial commit

--- src/js/game.js
+++ src/js/game.js
@@ -1,20 +1,79 @@
 export class Game {
-    constructor(canvasId) {
-        this.canvasId = canvasId;
-
-        this.canvas = document.getElementById(this.canvasId);
-        this.lines = [1,2,3,4];
-
-        this.lines.push('');
+    
+    constructor() {
+  
+        this.canvas = document.createElement("CANVAS");
         this.ctx = this.canvas.getContext('2d');
+        this.canvas.width=750;
+        this.canvas.height=450;
+        this.enemy=[];
     }
 
     start() {
         this.initialize();
     };
 
+    draw() {
+        // Clear canvas
+        this.ctx.fillStyle = "white"
+        this.ctx.fillRect(0, 0, canvas.width, canvas.height)
+      
+        // Render all objects in scene
+        for (i in scene) {
+          scene[i].draw()
+        }
+      }
+
     initialize() {
-        this.ctx.fillStyle = "green";
+
+
+        var map = [
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [2,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+
+          ];
+
+        this.ctx.fillStyle = "black";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    };
+        document.body.appendChild(this.canvas);
+
+        
+
+        var counterx=0;
+        var countery=0;
+        for (var y=0; y< this.canvas.height; y+=50) {
+            for (var x=0; x < this.canvas.width; x+=50) {
+                
+            //this.ctx.fillStyle='#'+Math.floor(Math.random()*16777215).toString(16);
+                 if(map[countery][counterx]===0){this.ctx.fillStyle="black"}
+                 else if(map[countery][counterx]===1){this.ctx.fillStyle="white"}
+                 else if(map[countery][counterx]===2){this.ctx.fillStyle="green";}
+            this.ctx.fillRect(x, y, 50, 50);
+            counterx++;
+
+            }
+            counterx=0;
+            countery++;
+        }
+
+       
+
+
+        // var img = new Image();
+        // img.src='tileRoadG.png';
+        // //img.src='https://mdn.mozillademos.org/files/5397/rhino.jpg';
+        // this.ctx.drawImage(img, 10, 10);
+
+        // var img2 = new Image();
+        // img2.src='https://mdn.mozillademos.org/files/5397/rhino.jpg';
+        // this.ctx.drawImage(img2 ,10, 10);
+
+    }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,246 @@
-import { Game } from './game';
+// import { Game } from './game';
 
-const game = new Game('myCanvas');
 
-game.start();
+// const game = new Game();
 
+// game.start();
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var background2;
+var map=[];
+var enemies=[];
+var towers=[];
+var projectiles=[];
+var sizeTile=50;
+var tick=0;
+
+function render(){
+    drawMap();
+    title();
+
+}
+function update(){
+    generateTower();
+    generateEnemy();
+    shoot();
+}
+function title()
+{
+    context.fillStyle = "red";
+    context.font = "40px Comic Sans MS";
+    context.textAlign = "center";
+    context.fillText('Tower', 640, 50);
+}
+class Tile
+{
+    constructor(x, y,size,identity)
+    {
+        this.x = x;
+        this.y = y;
+        this.size=size;
+        this.image = new Image;
+        this.image.src="";
+        this.speed = 0.5;
+        this.rotation = 0;
+        this.identity=identity;
+        this.matchAsset();
+    }
+
+    matchAsset(){
+        switch(this.identity){
+            case 0: this.image.src="tileRoad.png"; this.speed=0; break;
+            case 1: this.image.src="tile.png"; this.speed=0; break;
+            case 2: this.image.src="turent1.png"; this.speed=0; break;
+            case 3: this.image.src="projectile1.png"; this.speed=5; break;
+        }
+    }
+    create(){
+            context.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
+  
+}
+
+class Enemy{
+    constructor(x, y,size,identity)
+    {
+        this.x = x;
+        this.y = y;
+        this.size=size;
+        this.image = new Image;
+        this.image.src="";
+        this.speed = 1;
+        this.identity=identity;
+        this.matchAsset();
+    }
+    matchAsset(){
+        switch(this.identity){
+            case 10: this.image.src="tileRedCircle.png"; this.speed=2; break;
+        }
+    }
+    move(){
+        if((this.x + this.size) >= 1250 || (this.x) < 0 ) this.speed *= -1;
+        this.x += this.speed;
+
+        context.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
+}
+class Projectile{
+    constructor(x, y,size,identity)
+    {
+        this.x = x;
+        this.y = y;
+        this.size=size;
+        this.image = new Image;
+        this.image.src="";
+        this.speed = 1;
+        this.rotation = 0;
+        this.identity=identity;
+        this.matchAsset();
+    }
+    
+    matchAsset(){
+        switch(this.identity){
+            case 3: this.image.src="projectile1.png"; this.speed=5; break;
+        }
+    }
+    acquireEnemy(objectX,objectY){
+        
+        
+
+        var toObjectX = objectX - this.x;
+        var toObjectY = objectY - this.y;
+        //this.rotation = Math.atan2(toObjectY, toObjectX);
+    
+        // Normalize
+        var toObjectLength = Math.sqrt(toObjectX * toObjectX + toObjectY * toObjectY);
+        toObjectX = toObjectX / toObjectLength;
+        toObjectY = toObjectY / toObjectLength;
+    
+        // Move towards the player
+        this.x += toObjectX * this.speed;
+        this.y += toObjectY * this.speed;
+
+
+          this.rotation = Math.atan2(toObjectY, toObjectX);
+          console.log(this.rotation);
+
+        //console.log("Object:"+ objectX + " "+objectY+" Enemy:"+ Math.round(this.x) + " "+Math.round(this.y));
+        if(objectX==Math.round(this.x) && objectY==Math.round(this.y)){return 1;}
+        else{
+            context.save();
+            //context.translate(this.x,this.y);
+             //context.rotate(this.rotation);
+             context.drawImage(this.image, this.x, this.y, this.size, this.size);
+            context.restore();
+            return 0;
+        }
+       
+
+        //context.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
+
+
+
+}
+
+function generateMap(){
+
+    var map1=[
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    ];
+
+
+    for(var i=0;i<map1.length;i++){
+        for(var j=0;j<map1[0].length;j++){
+            map.push(new Tile(j*50,100+i*50,sizeTile,map1[i][j]));
+        }
+    }
+
+}
+
+function drawMap(){
+    context.clearRect(0, 0, 1280, 720);
+    context.rect(0, 0, 1280,720);
+    context.fillStyle = "black";
+    context.fill();
+    map.forEach(element => element.create());
+}
+function generateEnemy(){
+    
+
+    if(tick%100==0){
+        enemies.push(new Enemy(0,100,sizeTile,10));
+    }
+    enemies.forEach(element => element.move());
+}
+
+
+
+function shoot(){
+
+    if(tick%100==0){
+        towers.forEach(element => projectiles.push(new Projectile(element.x,element.y,sizeTile,3)));
+    }
+    if(enemies.length>0){
+    /*var max = enemies.reduce(function(prev, current) {
+        return (prev.x > current.x) ? prev : current
+    });
+    console.log(max);
+    */
+   var counter=0;
+    projectiles.forEach(element => {
+
+        if(element.acquireEnemy(enemies[0].x,enemies[0].y)==1){console.log("Hit");projectiles.splice(counter,1); enemies.splice(0,1);}
+        // else console.log("Nic");
+        // console.log(counter);
+        counter++;
+    });
+}
+}
+
+
+
+function generateTower(){
+    towers.forEach(element => element.create());
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
+  canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    console.log(message);  
+    towers.push(new Tile((mousePos.x-(mousePos.x%50)),(mousePos.y-(mousePos.y%50)),sizeTile,2));
+    }, false);
+
+    window.onload = function()
+{   
+    this.generateMap();
+    main();
+}
+
+function main()
+{
+    tick++;
+    render();
+    update();
+    requestAnimationFrame(main);
+}
